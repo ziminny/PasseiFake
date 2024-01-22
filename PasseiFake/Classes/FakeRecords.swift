b1e6c003eb5b12c84964f0b6d412bb09cb0411ee
Vagner Oliveira <ziminny@gmail.com>
2024-01-21 21:47:08 -0300

    Documentation

--- PasseiFake/Classes/FakeRecords.swift
+++ PasseiFake/Classes/FakeRecords.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 
+/// Classe responsável por gerar dados falsificados para testes ou outros propósitos.
 public class FakeRecords {
-    public func getRandomValue<T:FalsifiedDataProtocol>(ofType type:T.Type) -> T.Value {
+    /// Obtém um valor falsificado de um tipo que conforma ao protocolo `FalsifiedDataProtocol`.
+    ///
+    /// - Parameters:
+    ///   - type: O tipo que conforma ao protocolo `FalsifiedDataProtocol`.
+    /// - Returns: O valor falsificado gerado.
+    ///
+    /// - Note: Este método cria uma instância do tipo fornecido e retorna o valor falsificado gerado pela propriedade `value` do tipo conformado.
+    public func getRandomValue<T: FalsifiedDataProtocol>(ofType type: T.Type) -> T.Value {
         let instance = type.init()
         return instance.value
     }
 
+    /// Inicializa uma instância da classe `FakeRecords`.
     public init() {}
+    
 }
+
